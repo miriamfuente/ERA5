d9090f78320b1071b57614d20c2ba1c747f3e029
Miriam Fuente <fuentesm@ui.sci.unican.es>
2024-11-28 11:34:51 +0000

    comments have been added

--- quantiles/load.transform.data.R
+++ quantiles/load.transform.data.R
@@ -1,4 +1,5 @@
 options(java.parameters = "-Xmx32g") 
+# Libraries needed for the script
 library(loadeR)
 library(transformeR)
 library(visualizeR)
@@ -13,45 +14,41 @@ library(climate4R.indices) # needed for calculate indices
 df <- "/lustre/gmeteo/PTICLIMA/DATA/REANALYSIS/ERA5/data/global/0.25/ERA5_025.ncml"
 mask.path <- "/lustre/gmeteo/PTICLIMA/DATA/REANALYSIS/ERA5/lsm/lsm_era5.nc"
 hottest.month <- readRDS("hottest.month/idx.hottest.month.masked.rds")
-latitud <- c(35.9, 44)
-longitud <- c(-9.6, 3.6)
+
+# latitude, longitude and years of the region of interest
+latitude <- c(35.9, 44)
+longitude <- c(-9.6, 3.6)
 years = 1986:2005
-dataset <- loadGridData(df, var="tp", latLim=latitud, lonLim=longitud, years=years)
-dataset <- gridArithmetics(dataset, 1000, operator="*")
+# tp for precipitation
+# tx2m for maximum temperature
+dataset <- loadGridData(df, var="tp", latLim=latitude, lonLim=longitude, years=years)
+dataset <- gridArithmetics(dataset, 1000, operator="*") # Convert to mm (Precipitation)
+# dataset <- gridArithmetics(dataset, 273, operator="-") # Convert to Celsius (Temperature)
 dataset$Data[dataset$Data < 0] <- 0
 
-# mask <- loadGridData(mask.path, var="lsm", latLim=c(42.3, 43.9), lonLim=c(-3.4,0.4))
-# # Values under 0.5 are considered as sea
-# mask$Data[mask$Data < 0.5] <- 0
-# mask$Data[mask$Data >= 0.5] <- 1
-# # Values of sea are set to NA
-# mask$Data[mask$Data == 0] <- NA
+# Subset the hottest month for the region of interest
+hottest.month.pi <- subsetGrid(hottest.month, latLim=latitude, lonLim=longitude)
 
-# mask$Data <- array(rep(mask$Data, each = dim(dataset$Data)[which(attr(dataset$Data, "dimensions") == "time")]),
-#                    dim = c(dim(dataset$Data)[which(attr(dataset$Data, "dimensions") == "time")], 
-#                            dim(mask$Data)[which(attr(mask$Data, "dimensions") == "lat")],
-#                            dim(mask$Data)[which(attr(mask$Data, "dimensions") == "lon")]))
-# attr(mask$Data, "dimensions") <- c("time", "lat", "lon")
-
-# land <- gridArithmetics(dataset, mask, operator="*")
-
-hottest.month.pi <- subsetGrid(hottest.month, latLim=latitud, lonLim=longitud)
+# Create lists to store the datasets
 list.lon <- list()
 list.lat <- list()
 list.total <- list()
+# Define the months
 meses <- 1:12
 
+# Loop over the latitude and longitude to mantein the hottest month and set to NA the rest of the months 
+# for the region of interest and keep the same structure of the original dataset
 for (i in 1:dim(dataset$Data)[which(attr(dataset$Data, "dimensions") == "lat")]) {
     dataset.lat <- subsetDimension(dataset, dimension="lat", indices=i)
   for (j in 1:dim(dataset$Data)[which(attr(dataset$Data, "dimensions") == "lon")]) {
     dataset.lon <- subsetDimension(dataset.lat, dimension="lon", indices=j)
     month <- hottest.month.pi$Data[i, j]
     if (is.na(month)) {
-      # Si month es NA, asignar NA a todos los valores de dataset.lon$Data y continuar
+      # If month is NA, assign NA to all values of dataset.lon$Data and continue
       dataset.lon$Data[!is.na(dataset.lon$Data)] <- NA  # Asigna NA a todos los elementos
       list.lon[[j]] <- dataset.lon
     } else {
-      # Procesamiento normal si month tiene un valor válido
+      # Normal processing if month has a valid value
       dataset.no.hottest <- subsetGrid(dataset.lon, season=c(setdiff(meses, month)))
       dataset.no.hottest$Data[!is.na(dataset.no.hottest$Data)] <- NA
       dataset.hottest <- subsetGrid(dataset.lon, season=month)
@@ -65,12 +62,12 @@ for (i in 1:dim(dataset$Data)[which(attr(dataset$Data, "dimensions") == "lat")])
 }
 list.total <- redim(do.call(bindGrid, c(list.lat, list(dimension = "lat", skip.temporal.check = TRUE))), drop=TRUE)
 
-a <- climatology(list.total)
-
+# Plot the mean of the dataset obtained
 png("pruebapirineosymas.tmax.png", width = 800, height = 600)
 spatialPlot(climatology(tmax), backdrop.theme = "coastline", color.theme="YlOrRd", rev.colors = FALSE, 
             main= "Daily maximum temperature")
 dev.off()
 
+# Save the dataset obtained
 saveRDS(list.total, "pr.daily.hottest.month.pi.rds", compress="xz")
 
